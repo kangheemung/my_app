d67129a20ea0beb32530c689692a4497545f6aa0
EC2 Default User <ec2-user@ip-10-0-0-172.ap-northeast-1.compute.internal>
2023-10-12 10:46:30 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { ColoredMessage } from "./components/ColoredMessage";
+import { useState, useEffect } from "react";
 
-function App() {
+export const App = () => {
+  useEffect(() => {
+    console.log("レンダリング");
+  }, []);
+  const [num, setNum] = useState(0);
+  const [title, setTitle] = useState("Appのタイトル!!");
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{title}</div>
+      <div className="container">
+        <p>エリア1です。</p>
+        <button onClick={onClickButton}>ボタン</button>
+      </div>
+      <div className="container">
+        <p>エリア2です。</p>
+      </div>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue">こんにちは！</ColoredMessage>
+      <ColoredMessage color="pink">お元気ですか？</ColoredMessage>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ColoredMessage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ColoredMessage = ({ message, color }) => {
+
+    return (<p style={{ color }}>{message}</p>);
+};
+
